a33a204e2ba02127040ed7ca7726925a57f0735d
geauxvirtual <justin.guidroz@gmail.com>
2015-09-03 10:50:12 -0700

    Start adding unit tests for influxdb plugin

---
+++ influx/influx_test.go
@@ -0,0 +1,17 @@
+package influx
+
+import (
+	"testing"
+
+	"github.com/intelsdi-x/pulse/control/plugin"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestInfluxDBPlugin(t *testing.T) {
+	Convey("Meta should return metadata for the plugin", t, func() {
+		meta := Meta()
+		So(meta.Name, ShouldResemble, name)
+		So(meta.Version, ShouldResemble, version)
+		So(meta.Type, ShouldResemble, plugin.PublisherPluginType)
+	})
+}
