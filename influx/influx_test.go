9ab49e9c1e32813251143f081446e1b279dfd0a7
geauxvirtual <justin.guidroz@gmail.com>
2015-09-09 10:15:45 -0700

    Add unit tests

--- influx/influx_test.go
+++ influx/influx_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/intelsdi-x/pulse/control/plugin"
+	"github.com/intelsdi-x/pulse/control/plugin/cpolicy"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -16,5 +17,24 @@ func TestInfluxDBPlugin(t *testing.T) {
 		So(meta.Name, ShouldResemble, name)
 		So(meta.Version, ShouldResemble, version)
 		So(meta.Type, ShouldResemble, plugin.PublisherPluginType)
+	})
+
+	Convey("Create InfluxPublisher", t, func() {
+		ip := NewInfluxPublisher()
+		Convey("So ip should not be nil", func() {
+			So(ip, ShouldNotBeNil)
+		})
+		Convey("So ip should be of influxPublisher type", func() {
+			So(ip, ShouldHaveSameTypeAs, &influxPublisher{})
+		})
+		Convey("ip.GetConfigPolicyNode() should return a config policy node", func() {
+			config := ip.GetConfigPolicyNode()
+			Convey("So config should not be nil", func() {
+				So(config, ShouldNotBeNil)
+			})
+			Convey("So config should be a cpolicy.ConfigPolicyNode", func() {
+				So(config, ShouldHaveSameTypeAs, cpolicy.ConfigPolicyNode{})
+			})
+		})
 	})
 }
