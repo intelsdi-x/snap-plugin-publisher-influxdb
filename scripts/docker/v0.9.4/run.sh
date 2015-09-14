12cd84ebe7c68577454ef076b59be4e9dbdc0643
geauxvirtual <justin.guidroz@gmail.com>
2015-09-14 16:16:35 -0700

    Update InfluxDB client libraries to 0.9.4 and update integration testing to Influx 0.9.4

--- scripts/docker/v0.9.4/run.sh
+++ scripts/docker/v0.9.4/run.sh
@@ -7,24 +7,24 @@ CONFIG_FILE="/config/config.toml"
 sed -i "s/^max-open-shards.*/max-open-shards = $(ulimit -n)/" ${CONFIG_FILE}
 
 # Configure InfluxDB Cluster
-if [ -n "${FORCE_HOSTNAME}" ]; then
-    if [ "${FORCE_HOSTNAME}" == "auto" ]; then
-        #set hostname with IPv4 eth0
-        HOSTIPNAME=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
-        /usr/bin/perl -p -i -e "s/^# hostname.*$/hostname = \"${HOSTIPNAME}\"/g" ${CONFIG_FILE}
-    else
-        /usr/bin/perl -p -i -e "s/^# hostname.*$/hostname = \"${FORCE_HOSTNAME}\"/g" ${CONFIG_FILE}
-    fi
-fi
+#if [ -n "${FORCE_HOSTNAME}" ]; then
+#    if [ "${FORCE_HOSTNAME}" == "auto" ]; then
+#        #set hostname with IPv4 eth0
+#        HOSTIPNAME=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
+#        /usr/bin/perl -p -i -e "s/^# hostname.*$/hostname = \"${HOSTIPNAME}\"/g" ${CONFIG_FILE}
+#    else
+#        /usr/bin/perl -p -i -e "s/^# hostname.*$/hostname = \"${FORCE_HOSTNAME}\"/g" ${CONFIG_FILE}
+#    fi
+#fi
 
-if [ -n "${SEEDS}" ]; then
-    SEEDS=$(eval SEEDS=$SEEDS ; echo $SEEDS | grep '^\".*\"$' || echo "\""$SEEDS"\"" | sed -e 's/, */", "/g')
-    /usr/bin/perl -p -i -e "s/^# seed-servers.*$/seed-servers = [${SEEDS}]/g" ${CONFIG_FILE}
-fi
+#if [ -n "${SEEDS}" ]; then
+#    SEEDS=$(eval SEEDS=$SEEDS ; echo $SEEDS | grep '^\".*\"$' || echo "\""$SEEDS"\"" | sed -e 's/, */", "/g')
+#    /usr/bin/perl -p -i -e "s/^# seed-servers.*$/seed-servers = [${SEEDS}]/g" ${CONFIG_FILE}
+#fi
 
-if [ -n "${REPLI_FACTOR}" ]; then
-    /usr/bin/perl -p -i -e "s/replication-factor = 1/replication-factor = ${REPLI_FACTOR}/g" ${CONFIG_FILE}
-fi
+#if [ -n "${REPLI_FACTOR}" ]; then
+#    /usr/bin/perl -p -i -e "s/replication-factor = 1/replication-factor = ${REPLI_FACTOR}/g" ${CONFIG_FILE}
+#fi
 
 if [ "${PRE_CREATE_DB}" == "**None**" ]; then
     unset PRE_CREATE_DB
@@ -39,21 +39,21 @@ if [ "${SSL_SUPPORT}" == "**False**" ]; then
 fi
 
 # Add Graphite support
-if [ -n "${GRAPHITE_DB}" ]; then
-    sed -i -r -e "/^\s+\[input_plugins.graphite\]/, /^$/ { s/false/true/; s/#//g; s/\"\"/\"${GRAPHITE_DB}\"/g; }" ${CONFIG_FILE}
-fi
+#if [ -n "${GRAPHITE_DB}" ]; then
+#    sed -i -r -e "/^\s+\[input_plugins.graphite\]/, /^$/ { s/false/true/; s/#//g; s/\"\"/\"${GRAPHITE_DB}\"/g; }" ${CONFIG_FILE}
+#fi
 
-if [ -n "${GRAPHITE_PORT}" ]; then
-    sed -i -r -e "/^\s+\[input_plugins.graphite\]/, /^$/ { s/2003/${GRAPHITE_PORT}/; }" ${CONFIG_FILE}
-fi
+#if [ -n "${GRAPHITE_PORT}" ]; then
+#    sed -i -r -e "/^\s+\[input_plugins.graphite\]/, /^$/ { s/2003/${GRAPHITE_PORT}/; }" ${CONFIG_FILE}
+#fi
 
 # Add UDP support
-if [ -n "${UDP_DB}" ]; then
-    sed -i -r -e "/^\s+\[input_plugins.udp\]/, /^$/ { s/false/true/; s/#//g; s/\"\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
-fi
-if [ -n "${UDP_PORT}" ]; then
-    sed -i -r -e "/^\s+\[input_plugins.udp\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
-fi
+#if [ -n "${UDP_DB}" ]; then
+#    sed -i -r -e "/^\s+\[input_plugins.udp\]/, /^$/ { s/false/true/; s/#//g; s/\"\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
+#fi
+#if [ -n "${UDP_PORT}" ]; then
+#    sed -i -r -e "/^\s+\[input_plugins.udp\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
+#fi
 
 # Pre create database on the initiation of the container
 API_URL="http://localhost:8086"
