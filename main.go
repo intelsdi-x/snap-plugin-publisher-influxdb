e9676ee86243fe29fb650d211978d1b3ce50ed73
geauxvirtual <justin.guidroz@gmail.com>
2015-10-13 09:28:25 -0700

    Update import path for influx package

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ package main
 import (
 	"os"
 
+	"github.com/intelsdi-x/pulse-plugin-publisher-influxdb/influx"
 	"github.com/intelsdi-x/pulse/control/plugin"
-	"github.com/intelsdi-x/pulse/plugin/publisher/pulse-publisher-influxdb/influx"
 )
 
 func main() {
