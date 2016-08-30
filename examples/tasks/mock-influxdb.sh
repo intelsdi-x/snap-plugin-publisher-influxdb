f25923a73c5fac243d3c30c14492a99453b75d85
Nichelle A Hall <nichelle.a.hall@gmail.com>
2016-08-30 16:00:14 -0700

    Fixed error with " in SNAP_VERSION and allowed for snapctl to download before loading plugins

--- examples/tasks/mock-influxdb.sh
+++ examples/tasks/mock-influxdb.sh
@@ -23,10 +23,34 @@ _info "Get latest plugins"
 _info "creating database"
 curl -i -XPOST http://influxdb:8086/query --data-urlencode "q=CREATE DATABASE test"
 
-_info "loading plugins"
-snapctl plugin load "${PLUGIN_PATH}/snap-plugin-publisher-mock-file"
-snapctl plugin load "${PLUGIN_PATH}/snap-plugin-collector-mock2"
-snapctl plugin load "${PLUGIN_PATH}/snap-plugin-publisher-influxdb" 
+SNAP_FLAG=0
 
-_info "creating and starting a task"
-snapctl task create -t "${__dir}/task-mock-influxdb.yml" 
+# this block will wait check if snapctl and snapd are loaded before the plugins are loaded and the task is started
+ for i in `seq 1 5`; do
+             if [[ -f /usr/local/bin/snapctl && -f /usr/local/bin/snapd ]];
+                then
+
+                    _info "loading plugins"
+                    snapctl plugin load "${PLUGIN_PATH}/snap-plugin-publisher-mock-file"
+                    snapctl plugin load "${PLUGIN_PATH}/snap-plugin-collector-mock2"
+                    snapctl plugin load "${PLUGIN_PATH}/snap-plugin-publisher-influxdb" 
+
+                    _info "creating and starting a task"
+                    snapctl task create -t "${__dir}/task-mock-influxdb.yml" 
+
+                    SNAP_FLAG=1
+
+                    break
+             fi 
+        
+        _info "snapctl and/or snapd are unavailable, sleeping for 3 seconds" 
+        sleep 3
+done 
+
+
+# check if snapctl/snapd have loaded
+if [ $SNAP_FLAG -eq 0 ]
+    then
+     echo "Could not load snapctl or snapd"
+     exit 1
+fi
