0484bcf35d9c367c45fb5a60a3d16a3b447353ef
Marcin Krolik <marcin.krolik@intel.com>
2016-11-23 15:31:32 +0100

    Updated Snap binaries in large tests and in example

--- examples/tasks/mock-influxdb.sh
+++ examples/tasks/mock-influxdb.sh
@@ -16,7 +16,6 @@ PLUGIN_PATH=${PLUGIN_PATH:-"${TMPDIR}/snap/plugins"}
 mkdir -p $PLUGIN_PATH
 
 _info "Get latest plugins"
-(cd $PLUGIN_PATH && curl -sfLSO http://snap.ci.snap-telemetry.io/snap/master/latest/snap-plugin-publisher-mock-file && chmod 755 snap-plugin-publisher-mock-file)
 (cd $PLUGIN_PATH && curl -sfLSO http://snap.ci.snap-telemetry.io/snap/master/latest/snap-plugin-collector-mock2 && chmod 755 snap-plugin-collector-mock2)
 (cd $PLUGIN_PATH && curl -sfLSO http://snap.ci.snap-telemetry.io/plugins/snap-plugin-publisher-influxdb/latest_build/linux/x86_64/snap-plugin-publisher-influxdb && chmod 755 snap-plugin-publisher-influxdb)
 
@@ -25,32 +24,31 @@ curl -i -XPOST http://influxdb:8086/query --data-urlencode "q=CREATE DATABASE te
 
 SNAP_FLAG=0
 
-# this block will wait check if snapctl and snapd are loaded before the plugins are loaded and the task is started
+# this block will wait check if snaptel and snapteld are loaded before the plugins are loaded and the task is started
  for i in `seq 1 5`; do
-             if [[ -f /usr/local/bin/snapctl && -f /usr/local/bin/snapd ]];
+             if [[ -f /usr/local/bin/snaptel && -f /usr/local/sbin/snapteld ]];
                 then
 
                     _info "loading plugins"
-                    snapctl plugin load "${PLUGIN_PATH}/snap-plugin-publisher-mock-file"
-                    snapctl plugin load "${PLUGIN_PATH}/snap-plugin-collector-mock2"
-                    snapctl plugin load "${PLUGIN_PATH}/snap-plugin-publisher-influxdb" 
+                    snaptel plugin load "${PLUGIN_PATH}/snap-plugin-collector-mock2"
+                    snaptel plugin load "${PLUGIN_PATH}/snap-plugin-publisher-influxdb" 
 
                     _info "creating and starting a task"
-                    snapctl task create -t "${__dir}/task-mock-influxdb.yml" 
+                    snaptel task create -t "${__dir}/task-mock-influxdb.yml" 
 
                     SNAP_FLAG=1
 
                     break
              fi 
         
-        _info "snapctl and/or snapd are unavailable, sleeping for 3 seconds" 
-        sleep 3
+        _info "snaptel and/or snapteld are unavailable, sleeping for 3 seconds" 
+        sleep 10
 done 
 
 
-# check if snapctl/snapd have loaded
+# check if snaptel/snapteld have loaded
 if [ $SNAP_FLAG -eq 0 ]
     then
-     echo "Could not load snapctl or snapd"
+     echo "Could not load snaptel or snapteld"
      exit 1
 fi
