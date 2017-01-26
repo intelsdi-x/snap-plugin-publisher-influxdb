44cabf87df2de409ba59aa2a07bcc95afe88bd74
Nan Liu <nan.liu@gmail.com>
2017-01-25 20:18:38 -0800

    Update large test to fix false negative
    * Set max-failure to 1 so we detect failures
    * Add required rentention config in tasks
    * Disable Snap 1.0.0 tests since ENV var in tasks is not supported

--- scripts/test/large_spec.rb
+++ scripts/test/large_spec.rb
@@ -4,6 +4,8 @@ require_relative './spec_helper'
 require 'specinfra/backend/docker_compose'
 
 compose_yml = File.expand_path(File.join(__FILE__, "../docker-compose.yml"))
+raise(Exception, "Missing docker-compose file: #{compose_yml}") unless File.exists? compose_yml
+
 set :docker_compose_container, :snap
 
 describe docker_compose(compose_yml) do
@@ -18,7 +20,7 @@ describe docker_compose(compose_yml) do
         }
       end
 
-      if os[:family] != 'ubuntu'
+      if os[:family] == 'alpine'
         describe port(8181) do
           it { should be_listening }
         end
@@ -63,6 +65,8 @@ describe docker_compose(compose_yml) do
               id = subject.stdout.split("\n").find{|l|l=~/^ID:/}
               task_id = $1 if id.match(/^ID: (.*)$/)
               expect(task_id).to_not be_nil
+              # NOTE we need a short pause before checking task state in case it fails:
+              sleep 3
             }
           end
 
@@ -73,6 +77,8 @@ describe docker_compose(compose_yml) do
 
           describe "Metrics in running tasks" do
             it {
+              binding.pry if ENV["DEMO"] == "true"
+
               data = curl_json_api("http://127.0.0.1:8181/v1/tasks")
               task = data["body"]["ScheduledTasks"].find{|i| i['id'] == task_id}
               expect(task['id']).to eq task_id
